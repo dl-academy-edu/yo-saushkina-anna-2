e85614efdfe74148480e25e999a3c34161c7748c
AnnaSaushkina <saushkinanna@gmail.com>
2023-01-30 21:43:14 +0300

    Добавлено отключение скролла при открытом модальном окне

--- source/js/popup.js
+++ source/js/popup.js
@@ -4,13 +4,17 @@ const closePopup = document.querySelector('.close');
 const input = document.querySelector('.contact__input');
 const overlay = document.querySelector('.overlay');
 
+const btnOff = document.querySelector('.btn__js');
+const btnOn = document.querySelector('.popup');
+
+
 btn.addEventListener('click', function() {
     popup.classList.add('open');
     input.focus();
 });
 
 closePopup.addEventListener('click', () => {
-    popup.classList.remove('open');
+    popup.classList.remove('open'); 
 });
 
 overlay.addEventListener('click', () => {
@@ -23,3 +27,36 @@ window.addEventListener("keydown", function(e) {
         btn.focus();
     }
 });
+
+
+
+
+let disableScroll = function () {
+	let pagePosition = window.scrollY;
+	document.body.classList.add('disable-scroll');
+	document.body.dataset.position = pagePosition;
+
+}
+
+let enableScroll = function () {
+	let pagePosition = parseInt(document.body.dataset.position, 10);
+	document.body.style.top = 'auto';
+	document.body.classList.remove('disable-scroll');
+	window.scroll({ top: pagePosition, left: 0 });
+}
+
+btnOff.addEventListener('click', (e) => {
+	disableScroll();
+	e.currentTarget.style.pointerEvents = 'none';
+	btnOn.style.pointerEvents = 'auto';
+
+});
+
+btnOn.addEventListener('click', (e) => {
+	enableScroll();
+	e.currentTarget.style.pointerEvents = 'none';
+	btnOff.style.pointerEvents = 'auto';
+});
+
+
+	
